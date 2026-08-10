7e51d39c32c1448df604f00d821aaff8d72aa1e4
Your Name <abdulahad2086907@gmail.com>
2026-08-10 20:14:37 +0500

    Make Testimonials globe responsive

--- app/sections/Testimonials.jsx
+++ app/sections/Testimonials.jsx
@@ -116,7 +116,7 @@ export function Testimonials() {
           </div>
 
           <ScrollReveal delay={0.15} className="relative w-full flex justify-center items-center">
-            <div className="relative h-80 w-80 rounded-[1.5rem] overflow-hidden bg-surface/50 border border-border/50">
+            <div className="relative h-64 w-64 sm:h-72 sm:w-72 md:h-80 md:w-80 rounded-[1.5rem] overflow-hidden bg-surface/50 border border-border/50">
               <World data={sampleArcs} globeConfig={globeConfig} />
             </div>
           </ScrollReveal>
